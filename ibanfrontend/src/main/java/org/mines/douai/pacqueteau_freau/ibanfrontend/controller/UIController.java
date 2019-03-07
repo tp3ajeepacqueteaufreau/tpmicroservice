2c37d0bc30e7326ccbf3bf7ab00e0feeec101383
wfreau <wfreau@gmail.com>
2019-03-07 14:45:32 +0100

    iban frontend début

---
+++ ibanfrontend/src/main/java/org/mines/douai/pacqueteau_freau/ibanfrontend/controller/UIController.java
@@ -0,0 +1,14 @@
+package org.mines.douai.pacqueteau_freau.ibanfrontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UIController {
+    
+    @GetMapping("/iban")
+    public String getIban() {
+        return "index.html";
+    }
+    
+}
