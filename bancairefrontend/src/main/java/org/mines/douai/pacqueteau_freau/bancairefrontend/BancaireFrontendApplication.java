a7e6cec8da0c4697c4eb3dbdc4beedc5afcf6adf
wfreau <wfreau@gmail.com>
2019-03-09 15:46:26 +0100

    All service start (Eureka included) but the service does not register in...
    Blocked for Feign client, service registration should work!

--- bancairefrontend/src/main/java/org/mines/douai/pacqueteau_freau/bancairefrontend/BancaireFrontendApplication.java
+++ bancairefrontend/src/main/java/org/mines/douai/pacqueteau_freau/bancairefrontend/BancaireFrontendApplication.java
@@ -2,8 +2,10 @@ package org.mines.douai.pacqueteau_freau.bancairefrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BancaireFrontendApplication {
     
     public static void main(String[] args) {
