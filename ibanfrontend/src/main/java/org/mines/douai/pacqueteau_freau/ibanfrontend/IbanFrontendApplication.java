6e44b0064c56a9bc3a3bc7c9cf30dfda27233f9e
wfreau <wfreau@gmail.com>
2019-03-09 18:33:05 +0100

    Ajout du service eureka pour pour Feign! Le service discovery fonctionne!

--- ibanfrontend/src/main/java/org/mines/douai/pacqueteau_freau/ibanfrontend/IbanFrontendApplication.java
+++ ibanfrontend/src/main/java/org/mines/douai/pacqueteau_freau/ibanfrontend/IbanFrontendApplication.java
@@ -2,8 +2,10 @@ package org.mines.douai.pacqueteau_freau.ibanfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class IbanFrontendApplication {
     
     public static void main(String[] args) {
