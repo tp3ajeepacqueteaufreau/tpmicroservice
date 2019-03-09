0c81339f18c0504e7e8feb64abd08898f7f972ab
wfreau <wfreau@gmail.com>
2019-03-09 19:38:30 +0100

    Enabling h2 database
    localhost:8083/h2
    
    user admin
    password <leave empty>

--- bancairebackend/src/main/java/org/mines/douai/pacqueteau_freau/bancairebackend/BancairebackendApplication.java
+++ bancairebackend/src/main/java/org/mines/douai/pacqueteau_freau/bancairebackend/BancairebackendApplication.java
@@ -3,9 +3,11 @@ package org.mines.douai.pacqueteau_freau.bancairebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class BancairebackendApplication {
     
     public static void main(String[] args) {
